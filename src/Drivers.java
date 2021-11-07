2b9c687352df036a7af9f91787f8fde3d1bcb02c
KatherineOracle <katherine@rasc.co.za>
2021-11-07 13:24:51 +0200

    Lint clean up - 1 issue remaining

--- src/Drivers.java
+++ src/Drivers.java
@@ -5,108 +5,101 @@ import java.util.Scanner; // Import the Scanner class to read text files
 import java.util.Comparator;
 import java.util.Formatter;
 import java.util.stream.Collectors;
-
 import java.util.List;
 import java.util.Optional;
 
 public class Drivers {
 
-  //properties
-  private List < Driver > drivers = new ArrayList < Driver > ();
+	// properties
+	private List<Driver> driverList = new ArrayList<>();
 
-  //constructor	
-  public Drivers() {
+	// constructor
+	public Drivers() {
 
-    // read through the driver text file and create a driver object from each row
+		// read through the driver text file and create a driver object from each row
 
-    try {
-      File driverSrc = new File("data/drivers-info.txt");
-      Scanner sc = new Scanner(driverSrc);
+		try {
+			File driverSrc = new File("data/drivers-info.txt");
+			Scanner sc = new Scanner(driverSrc);
 
-      while (sc.hasNextLine()) {
-        //read the line
-        String driverString = sc.nextLine();
+			while (sc.hasNextLine()) {
+				// read the line
+				String driverString = sc.nextLine();
 
-        //split the line of text into an array at the comma
-        String[] driverArr = driverString.split(", ");
+				// split the line of text into an array at the comma
+				String[] driverArr = driverString.split(", ");
 
-        //create the driver and add it to our ArrayList of drivers
-        drivers.add(new Driver(driverArr[0], driverArr[1], Integer.parseInt(driverArr[2])));
+				// create the driver and add it to our ArrayList of drivers
+				driverList.add(new Driver(driverArr[0], driverArr[1], Integer.parseInt(driverArr[2])));
 
-      }
+			}
 
-      //close scanner as we should
-      sc.close();
+			// close scanner as we should
+			sc.close();
 
-    } catch (FileNotFoundException e) {
+		} catch (FileNotFoundException e) {
 
-      System.out.println("Driver file not found");
+			System.out.println("Driver file not found");
 
-    }
+		}
 
-  }
+	}
 
-  //method to check if there are any drivers in the requested city
-  public boolean isAvailable(String textRestaurantCity) {
+	// method to check if there are any drivers in the requested city
+	public boolean isAvailable(String textRestaurantCity) {
 
-    List < Driver > drivers = this.drivers;
+		List<Driver> drivers = this.driverList;
 
-    // use an optional in case the result of the filter is null.
-    Optional < Driver > sortedList = drivers.stream()
+		// use an optional in case the result of the filter is null.
+		Optional<Driver> sortedList = drivers.stream()
 
-      //sort the drivers according to their load
-      .sorted(Comparator.comparing(Driver::getLoad))
+				// sort the drivers according to their load
+				.sorted(Comparator.comparing(Driver::getLoad))
 
-      //only return drivers with same city
-      .filter(d -> d.getCity().equals(textRestaurantCity))
+				// only return drivers with same city
+				.filter(d -> d.getCity().equals(textRestaurantCity))
 
-      //are their any results?
-      .findAny();
+				// are their any results?
+				.findAny();
 
-    return sortedList.isPresent();
-  }
+		return sortedList.isPresent();
+	}
 
-  private void saveToFile() {
-    try {
-      //dump the string into a file
-      Formatter writer = new Formatter("data/drivers.txt");
+	private void saveToFile() {
+		try (Formatter writer = new Formatter("data/drivers.txt")) {
+			// dump the string into a file
 
-      this.drivers.forEach((driver) -> {
-        writer.format("%s%n", driver.toString());
-      });
+			this.driverList.forEach(driver -> writer.format("%s%n", driver.toString()));
 
-      writer.close();
+		} catch (Exception e) {
 
-    } catch (Exception e) {
+			// Something went wrong
+			// log error
 
-      //Something went wrong
-      // log  error
+		}
+	}
 
-    }
+	// method to return driver in the selected city with the smallest load
+	public Driver appoint(String textRestaurantCity) {
 
-  }
+		// create a list drivers in matching city, ordered by load
+		List<Driver> sortedList;
+		sortedList = driverList.stream().sorted(Comparator.comparing(Driver::getLoad))
+				.filter(d -> d.getCity().equals(textRestaurantCity))
+				// Linter suggestion breaks the code. Help???
+				.collect(Collectors.toList());
 
-  //method to return driver in the selected city with the smallest load
-  public Driver appoint(String textRestaurantCity) {
-    List < Driver > drivers = this.drivers;
+		// get the driver at top of list
+		Driver appointed = sortedList.get(0);
 
-    // create a list  drivers in matching city, ordered by load
-    List < Driver > sortedList = drivers.stream()
-      .sorted(Comparator.comparing(Driver::getLoad))
-      .filter(d -> d.getCity().equals(textRestaurantCity))
-      .collect(Collectors.toList());
+		// update driver load
+		appointed.addToLoad();
 
-    //get the driver at top of list
-    Driver appointed = sortedList.get(0);
+		// save the driver file
+		this.saveToFile();
 
-    //update driver load
-    appointed.addToLoad();
-
-    //resave the driver file
-    this.saveToFile();
-
-    //send selected driver object back to main app
-    return appointed;
-  }
+		// send selected driver object back to main app
+		return appointed;
+	}
 
 }
